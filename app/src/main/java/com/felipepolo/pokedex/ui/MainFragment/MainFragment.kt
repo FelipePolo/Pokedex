e227dfe3853d7b28113838dab567fc7a64622469
FelipePolo <felipenv755@gmail.com>
2021-09-14 15:38:35 -0500

    preview added

--- app/src/main/java/com/felipepolo/pokedex/ui/MainFragment/MainFragment.kt
+++ app/src/main/java/com/felipepolo/pokedex/ui/MainFragment/MainFragment.kt
@@ -22,16 +22,6 @@ import com.google.android.material.tabs.TabLayoutMediator
 import dagger.android.support.DaggerFragment
 import javax.inject.Inject
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MainFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MainFragment : DaggerFragment() {
 
     @Inject
